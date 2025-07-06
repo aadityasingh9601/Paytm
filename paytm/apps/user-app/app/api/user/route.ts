f7106e235804c419f0fcd1cf36b2e34128a08613
Aaditya Singh <aadityasingh9601@gmail.com>
2025-07-06 18:36:24 +0530

    Added session providers & getSession route

---
+++ paytm/apps/user-app/app/api/user/route.ts
@@ -0,0 +1,23 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../../lib/auth";
+
+//This function returns the session details of the current logged in user.
+
+export const GET = async () => {
+  try {
+    const session = await getServerSession(authOptions);
+    if (session?.user) {
+      return NextResponse.json({
+        user: session?.user,
+      });
+    }
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: "You aren't logged in",
+      },
+      { status: 403 }
+    );
+  }
+};
