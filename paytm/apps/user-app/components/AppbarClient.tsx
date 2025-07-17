7378c8a0fcd3be160741265ee9e8c046650e699c
Aaditya Singh <aadityasingh9601@gmail.com>
2025-07-17 18:09:23 +0530

    Made button2.tsx, edited textInput.tsx

--- paytm/apps/user-app/components/AppbarClient.tsx
+++ paytm/apps/user-app/components/AppbarClient.tsx
@@ -1,18 +1,24 @@
-"use client"
+"use client";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "@repo/ui/appbar";
+import { Appbar } from "@repo/ui/Appbar";
 import { useRouter } from "next/navigation";
+//We've created this separate as in layout, we were facing error again & again, can't provide context to server componenents
+//etc, we can't make the root layout componenet a client component.
 
 export function AppbarClient() {
   const session = useSession();
   const router = useRouter();
 
   return (
-   <div>
-      <Appbar onSignin={signIn} onSignout={async () => {
-        await signOut()
-        router.push("/api/auth/signin")
-      }} user={session.data?.user} />
-   </div>
+    <div>
+      <Appbar
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut();
+          router.push("/api/auth/signin");
+        }}
+        user={session.data?.user}
+      />
+    </div>
   );
 }
