3b7105df7c6dbf68b4d4e77c8f234b3384ed8706
Aaditya Singh <aadityasingh9601@gmail.com>
2025-07-25 16:51:01 +0530

    cd

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -2,10 +2,9 @@ import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
-//This function returns the session details of the current logged in user.
-
 export const GET = async () => {
   try {
+    //This function returns the session details of the current logged-in user.
     const session = await getServerSession(authOptions);
     console.log(session);
     if (session?.user) {
@@ -16,7 +15,7 @@ export const GET = async () => {
   } catch (e) {
     return NextResponse.json(
       {
-        message: "You are not logged in",
+        message: "You aren't logged in",
       },
       { status: 403 }
     );
