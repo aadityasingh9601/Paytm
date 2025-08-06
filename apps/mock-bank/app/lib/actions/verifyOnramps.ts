7e6d1a8f87a5b91bda76f8aca48520f038fbc03d
Aaditya Singh <aadityasingh9601@gmail.com>
2025-08-06 20:26:56 +0530

    Build UI for mock bank service & created server actions

---
+++ apps/mock-bank/app/lib/actions/verifyOnramps.ts
@@ -0,0 +1,7 @@
+export const verifyOnramps = async (data: any) => {
+  console.log("inside onramps");
+  console.log(data);
+  return {
+    message: "Received your request",
+  };
+};
--- apps/mock-bank/app/netbanking/[id]/page.tsx
+++ apps/mock-bank/app/netbanking/[id]/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-import { Card } from "@repo/ui/card";
-import { TextInput } from "@repo/ui/TextInput";
-import { Button } from "@repo/ui/Button";
+import VerfiyTpin from "../../../components/VerifyTpin";
+import db from "@repo/db/client";
 
 //You can make it a async component now, to fetch the transaction data using the token, like u've done in p2p & transfer,
 //you can use the verifyTpin client component in this page & pass any data to it if necessary.
@@ -12,11 +10,31 @@ interface Props {
   };
 }
 
-export default function page({ params }: Props) {
-  const token = params;
+async function getTransactionData(token: string) {
+  //This function will fetch the current transaction's data, as that will be used to display info on the netbanking page.
+  const t = await db.onRampTransaction.findFirst({
+    where: {
+      token: token,
+    },
+  });
+
+  if (t)
+    return {
+      token: t.token,
+      amount: t.amount,
+      userId: t.userId,
+      provider: t.provider,
+    };
+}
+
+export default async function page({ params }: Props) {
+  const token = params.id;
+  const txnData = await getTransactionData(token);
   return (
     <div className="flex justify-center items-center h-[92.5vh]">
-      <div className="min-w-[40rem]"></div>
+      <div className="min-w-[40rem]">
+        <VerfiyTpin txn={txnData} />
+      </div>
     </div>
   );
 }
