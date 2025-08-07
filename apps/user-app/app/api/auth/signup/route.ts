a7686f001b1ea4585c99c1c3e79a494daaf995ac
Aaditya Singh <aadityasingh9601@gmail.com>
2025-08-07 18:20:21 +0530

    added Zod + React hook form validation to Signin & Signup

--- apps/user-app/app/api/auth/signup/route.ts
+++ apps/user-app/app/api/auth/signup/route.ts
@@ -2,18 +2,24 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import db from "@repo/db/client";
 import bcrypt from "bcrypt";
-import Email from "next-auth/providers/email";
-import { STATUS_CODES } from "http";
+import { signupInput } from "@repo/schema/schema";
+import { json } from "zod";
 
 //This function returns the session details of the current logged in user.
 
 export const POST = async (req: Request, res: NextApiResponse) => {
   try {
-    console.log("inside signup post route on backend.");
     const body = await req.json();
-    //Add zod validation here.
-    const { email, phone, password } = body;
     console.log(body);
+    const { email, phone, password } = body;
+    //Zod validation
+    const result = signupInput.safeParse(body);
+    if (!result.success) {
+      console.log(result.error); // ZodError instance
+      return new Response(result.error.message, {
+        status: 400,
+      });
+    }
 
     const existingUser = await db.user.findFirst({
       where: {
@@ -46,14 +52,14 @@ export const POST = async (req: Request, res: NextApiResponse) => {
       },
     });
 
-    return Response.json({
-      message: "User created successfully!",
+    return new Response("User created successfully", {
+      status: 200,
     });
   } catch (e) {
     console.log(e);
     return NextResponse.json(
       {
-        message: "Something went wrong! Try again",
+        message: "Something went wrong! Try again!",
       },
       { status: 400 }
     );
--- packages/ui/src/Button2.tsx
+++ packages/ui/src/Button2.tsx
@@ -2,17 +2,18 @@
 import { ReactNode } from "react";
 
 interface Button2Props {
-  onClick: () => void;
+  onClick?: () => void;
   children: ReactNode;
-  type: string;
+  type: "submit" | "reset" | "button";
+  shade: string;
 }
 
-export const Button2 = ({ onClick, children, type }: Button2Props) => {
+export const Button2 = ({ onClick, children, type, shade }: Button2Props) => {
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
-      className={`w-[20rem] rounded-full py-2.5 ${type == "solid" ? "bg-[#0074de] text-white" : "bg-white text-blue-500 border-[2px] border-[#0074de]"} `}
+      className={`w-[20rem] rounded-full py-2.5 ${shade === "solid" ? "bg-[#0074de] text-white" : "bg-white text-blue-500 border-[2px] border-[#0074de]"} `}
     >
       {children}
     </button>
