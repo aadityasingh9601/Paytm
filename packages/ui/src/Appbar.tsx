e2bd518ec4d7e1ce2dddb2bc243e6f1bf1e5f465
Aaditya Singh <aadityasingh9601@gmail.com>
2026-02-24 10:17:03 +0000

    Updated metadata & appbar

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -12,7 +12,9 @@ interface AppbarProps {
 export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
   return (
     <div className="flex justify-between border-b px-4 border-slate-300 bg-white">
-      <div className="text-lg flex flex-col justify-center">PayTM</div>
+      <div className="text-2xl text-[#6a51a6] font-bold flex flex-col justify-center">
+        DigiWallet
+      </div>
       <div className="flex flex-col justify-center pt-2">
         <Button onClick={user ? onSignout : onSignin} type="button">
           {user ? "Logout" : "Login"}
