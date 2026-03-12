6914af3d9d338e26535119a369463ac09daf8092
Aaditya Singh <aadityasingh9601@gmail.com>
2026-03-12 12:04:03 +0000

    added basic tests for signup page

---
+++ tests/signup.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+
+//Test to check the functionality of login page.
+
+test.describe("tests the signup page", () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto("http://localhost:3000/auth/signup");
+  });
+
+  test("basic functionality", async ({ page }) => {
+    await page.getByRole("textbox", { name: "Enter your email" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter your email" })
+      .fill("test@exampleee.com");
+    await page.getByRole("textbox", { name: "Enter phone number" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter phone number" })
+      .fill("123456788");
+    await page.getByRole("textbox", { name: "Enter password" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter password" })
+      .fill("12345678");
+    await page.getByRole("button", { name: "Next", exact: true }).click();
+    //If everything goes well
+    await expect(page).toHaveURL("http://localhost:3000/auth/signin");
+  });
+
+  test("navigation", async ({ page }) => {
+    await page
+      .getByRole("button", { name: "Existing account? Log in!" })
+      .click();
+
+    await expect(page).toHaveURL("http://localhost:3000/auth/signin");
+  });
+});
