918820718219ce077b43ed28c5794196185937d4
Aaditya Singh <aadityasingh9601@gmail.com>
2026-03-21 12:13:10 +0000

    added scripts to run E2E tests using Docker

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -1,6 +1,7 @@
 docker-compose -f docker-compose-test.yml up -d
 echo '🟡 - Waiting for database to be ready...'
-./wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
-npx prisma migrate deploy --name init
+./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/paytm" -- echo '🟢 - Database is ready!'
+DATABASE_URL=postgresql://postgres:mysecretpassword@localhost:5432/paytm \
+npx prisma migrate deploy --schema ./packages/db/prisma/schema.prisma
 npx playwright test
-docker-compose -f docker-compose.test.yml down
+docker-compose -f docker-compose-test.yml down
