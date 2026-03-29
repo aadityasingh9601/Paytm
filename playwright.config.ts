685ec8aba01144aa40424dae6fd9733eb16294d8
Aaditya Singh <aadityasingh9601@gmail.com>
2026-03-29 04:29:19 +0000

    added dotenv & test.yml workflow for running E2E testsin CI pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,18 +27,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
       name: "Google Chrome",
       use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    },
-
-    {
-      name: "Microsoft Edge",
-      use: { ...devices["Desktop Edge"], channel: "msedge" },
     },
 
     {
@@ -50,6 +40,16 @@ export default defineConfig({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
+
+    // {
+    //   name: "Microsoft Edge",
+    //   use: { ...devices["Desktop Edge"], channel: "msedge" },
+    // },
+
+    // {
+    //   name: "chromium",
+    //   use: { ...devices["Desktop Chrome"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -74,7 +74,7 @@ export default defineConfig({
 
   // Run your local dev server before starting the tests
   webServer: {
-    command: "npm run dev", // starts app with TEST database
+    command: "npm run dev:test", // starts app with TEST database
     url: "http://localhost:3000",
     reuseExistingServer: true, // locally reuse if already running, on CI always fresh
     timeout: 60 * 1000, //60 seconds
