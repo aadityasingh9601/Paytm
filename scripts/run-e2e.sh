29b52b3a7dc78c87c3c5dd65acfc9c081ee63d9b
Aaditya Singh <aadityasingh9601@gmail.com>
2026-03-30 05:11:54 +0000

    docker-compose change

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -1,7 +1,7 @@
-docker-compose -f docker-compose-test.yml up -d
+docker compose -f docker-compose-test.yml up -d
 echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/paytm_test_db" -- echo '🟢 - Database is ready!'
 DATABASE_URL=postgresql://postgres:mysecretpassword@localhost:5432/paytm_test_db \
 npx prisma migrate deploy --schema ./packages/db/prisma/schema.prisma
 npx playwright test
-docker-compose -f docker-compose-test.yml down
+docker compose -f docker-compose-test.yml down
