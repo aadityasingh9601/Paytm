c725c6fd61e595eb4c1e831aaaacfd172a600b3a
Aaditya Singh <aadityasingh9601@gmail.com>
2026-03-30 09:59:12 +0000

    added upload artifacts step in test.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import * as os from "node:os";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -12,7 +13,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 6 : 6,
+  workers: process.env.CI ? 1 : os.cpus().length,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
