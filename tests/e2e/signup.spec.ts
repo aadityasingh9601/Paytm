dcbc03e818149ce25a3b4f36c0c3b0d7014d100b
Aaditya Singh <aadityasingh9601@gmail.com>
2026-04-22 15:29:38 +0000

    cleanup

--- tests/e2e/signup.spec.ts
+++ tests/e2e/signup.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-//Test to check the functionality of login page.
-
 test.describe("tests the signup page", () => {
   test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
@@ -9,19 +7,18 @@ test.describe("tests the signup page", () => {
   });
 
   test("basic functionality", async ({ page }) => {
-    await page.getByRole("textbox", { name: "Enter your email" }).click();
-    await page
-      .getByRole("textbox", { name: "Enter your email" })
-      .fill(`test-${Date.now()}@abc.com`);
-    await page.getByRole("textbox", { name: "Enter phone number" }).click();
-    await page
-      .getByRole("textbox", { name: "Enter phone number" })
-      .fill(`${Math.floor(1000000000 + Math.random() * 9000000000)}`);
-    await page.getByRole("textbox", { name: "Enter password" }).click();
-    await page
-      .getByRole("textbox", { name: "Enter password" })
-      .fill("12345678");
-    await page.getByRole("button", { name: "Next", exact: true }).click();
+    const email = page.getByRole("textbox", { name: "Enter your email" });
+    const phone = page.getByRole("textbox", { name: "Enter phone number" });
+    const password = page.getByRole("textbox", { name: "Enter password" });
+    const submitBtn = page.getByRole("button", { name: "Next", exact: true });
+
+    await email.click();
+    await email.fill(`test-${Date.now()}@abc.com`);
+    await phone.click();
+    await phone.fill(`${Math.floor(1000000000 + Math.random() * 9000000000)}`);
+    await password.click();
+    await password.fill("12345678");
+    await submitBtn.click();
     //If everything goes well
     await expect(page).toHaveURL("http://localhost:3000/auth/signin");
   });
